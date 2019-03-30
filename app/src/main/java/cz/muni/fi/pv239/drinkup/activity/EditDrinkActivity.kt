bbb9631f03bba64342b0bb8d48435acc0376ed35
Martin Hamerník <445720@mail.muni.cz>
2019-03-30 11:59:46 +0100

    Add basic activities based on UI wireframe

---
+++ app/src/main/java/cz/muni/fi/pv239/drinkup/activity/EditDrinkActivity.kt
@@ -0,0 +1,13 @@
+package cz.muni.fi.pv239.drinkup.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import cz.muni.fi.pv239.drinkup.R
+
+class EditDrinkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_drink)
+    }
+}
