b3bcf0cb6c0483aa39d2dd2ae9783ebd30f75419
Martin Hamerník <445720@mail.muni.cz>
2019-03-30 17:34:54 +0100

    Add drawer with navigation to main activity. Replace some activities with fragments

--- app/src/main/java/cz/muni/fi/pv239/drinkup/activity/MainActivity.kt
+++ app/src/main/java/cz/muni/fi/pv239/drinkup/activity/MainActivity.kt
@@ -1,40 +1,145 @@
 package cz.muni.fi.pv239.drinkup.activity
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 import cz.muni.fi.pv239.drinkup.R
-
+import cz.muni.fi.pv239.drinkup.fragment.*
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity :
+    StatisticsFragment.OnStatisticsFragmentInteractionListener,
+    OverviewFragment.OnOverviewFragmentInteractionListener,
+    HistoryFragment.OnHistoryFragmentInteractionListener,
+    AchievementsFragment.OnAchievementsFragmentInteractionListener,
+    MyDrinksFragment.OnMyDrinksFragmentInteractionListener,
+    AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        createDrawer()
+        createAppBar()
+        if (savedInstanceState != null) {
+            showOverview()
         }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    override fun onOverviewFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStatisticsFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onHistoryFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onAchievementsFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onMyDrinksFragmentInteraction(uri: Uri) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun createDrawer() {
+        drawerLayout = findViewById(R.id.drawer_layout)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            handleNavMenuItemSelection(menuItem)
+        }
+        createAppBar()
+    }
+
+    private fun createAppBar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+    }
+
+    private fun handleNavMenuItemSelection(menuItem: MenuItem): Boolean {
+        menuItem.isChecked = true
+        drawerLayout.closeDrawers()
+        return when (menuItem.itemId) {
+            R.id.nav_overview -> {
+                showOverview()
+                true
+            }
+            R.id.nav_my_drinks -> {
+                showMyDrinks()
+                true
+            }
+            R.id.nav_history -> {
+                showHistory()
+                true
+            }
+            R.id.nav_achievements -> {
+                showAchievements()
+                true
+            }
+            R.id.nav_statistics -> {
+                showStatistics()
+                true
+            }
+            R.id.nav_settings -> {
+                showSettings()
+                true
+            }
+            else -> true
+        }
+    }
+
+    private fun showOverview() {
+        setActiveFragment(OverviewFragment())
+    }
+
+    private fun showMyDrinks() {
+        setActiveFragment(MyDrinksFragment())
+    }
+
+    private fun showAchievements() {
+        setActiveFragment(AchievementsFragment())
+    }
+
+    private fun showStatistics() {
+        setActiveFragment(StatisticsFragment())
+    }
+
+    private fun showHistory() {
+        setActiveFragment(HistoryFragment())
+    }
+
+    private fun showSettings() {
+        this.startActivity(Intent(this, SettingsActivity::class.java))
+    }
+
+    private fun setActiveFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+
 }
