8e056dc8769b3b0553130370679e970018dbe2a0
Martin Hamerník <445720@mail.muni.cz>
2019-04-04 23:30:59 +0200

    Add view components init to my drinks fragment

--- app/src/main/java/cz/muni/fi/pv239/drinkup/fragment/MyDrinksFragment.kt
+++ app/src/main/java/cz/muni/fi/pv239/drinkup/fragment/MyDrinksFragment.kt
@@ -1,6 +1,7 @@
 package cz.muni.fi.pv239.drinkup.fragment
 
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,46 +10,35 @@ import android.view.View
 import android.view.ViewGroup
 
 import cz.muni.fi.pv239.drinkup.R
+import cz.muni.fi.pv239.drinkup.activity.EditDrinkActivity
+import cz.muni.fi.pv239.drinkup.adapters.DrinksAdapter
+import cz.muni.fi.pv239.drinkup.database.entity.Drink
+import kotlinx.android.synthetic.main.fragment_my_drinks.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [MyDrinksFragment.OnMyDrinksFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [MyDrinksFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class MyDrinksFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var adapter: DrinksAdapter
+
     private var listenerMyDrinks: OnMyDrinksFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_drinks, container, false)
-    }
+        var view = inflater.inflate(R.layout.fragment_my_drinks, container, false)
+        my_drinks_list.adapter = adapter
 
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        listenerMyDrinks?.onMyDrinksFragmentInteraction(uri)
+        my_drinks_create_button.setOnClickListener {
+            var intent = Intent(it.context, EditDrinkActivity::class.java) // todo decide if reuse or use new activity for slightly different use case
+            it.context.startActivity(intent)
+        }
+        return view
     }
 
     override fun onAttach(context: Context) {
@@ -64,6 +54,19 @@ class MyDrinksFragment : Fragment() {
         super.onDetach()
         listenerMyDrinks = null
     }
+
+
+    fun loadDrinks() {
+        // todo load from DB
+    }
+
+    private fun populateList(drinks: List<Drink>?) {
+        if (drinks == null) {
+            return
+        }
+        adapter.refreshDrinks(drinks)
+    }
+
 
     /**
      * This interface must be implemented by activities that contain this
@@ -82,22 +85,8 @@ class MyDrinksFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MyDrinksFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MyDrinksFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance(): MyDrinksFragment {
+            return MyDrinksFragment()
+        }
     }
 }
