4af392eb511752a93b968e2133d870c0537d825f
Martin Hamerník <445720@mail.muni.cz>
2019-04-04 23:06:40 +0200

    Remove WIP object

--- app/src/main/java/cz/muni/fi/pv239/drinkup/utils/ApplicationContextProvider.kt
+++
@@ -1,9 +0,0 @@
-package cz.muni.fi.pv239.drinkup.utils
-
-import android.app.Application
-
-class ApplicationContextProvider(): Application() {
-    override fun onCreate() {
-        super.onCreate()
-    }
-}
