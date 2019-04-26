2542b621ce6bbdecf0173fb590ff8337b9fcd1e6
Martin Hamerník <445720@mail.muni.cz>
2019-04-26 13:43:07 +0200

    Prepare android app for communication with wearables

--- app/src/main/java/cz/muni/fi/pv239/drinkup/activity/MainActivity.kt
+++ app/src/main/java/cz/muni/fi/pv239/drinkup/activity/MainActivity.kt
@@ -12,16 +12,30 @@ import com.google.android.material.navigation.NavigationView
 import cz.muni.fi.pv239.drinkup.R
 import cz.muni.fi.pv239.drinkup.fragment.*
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.wearable.CapabilityClient
+import com.google.android.gms.wearable.CapabilityInfo
+import com.google.android.gms.wearable.Node
+import com.google.android.gms.wearable.Wearable
 
-class MainActivity :
+
+class MainActivity : AppCompatActivity(),
     StatisticsFragment.OnStatisticsFragmentInteractionListener,
     OverviewFragment.OnOverviewFragmentInteractionListener,
     HistoryFragment.OnHistoryFragmentInteractionListener,
     AchievementsFragment.OnAchievementsFragmentInteractionListener,
     MyDrinksFragment.OnMyDrinksFragmentInteractionListener,
-    AppCompatActivity() {
+    CapabilityClient.OnCapabilityChangedListener {
+
+    private var allConnectedNodes: List<Node> = Collections.emptyList()
+    private var wearNodesWithApp: Set<Node> = Collections.emptySet()
 
     private lateinit var drawerLayout: DrawerLayout
+
+    companion object {
+        @JvmStatic val CAPABILITY_WEAR_APP = "verify_remote_drinkup_wear_app"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +47,18 @@ class MainActivity :
         }
     }
 
+    override fun onPause() {
+        super.onPause()
+        Wearable.getCapabilityClient(this).removeListener(this, CAPABILITY_WEAR_APP)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Wearable.getCapabilityClient(this).addListener(this, CAPABILITY_WEAR_APP)
+        findWearDevicesWithApp()
+        findAllWearDevices()
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
@@ -41,6 +67,12 @@ class MainActivity :
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onCapabilityChanged(capabilityInfo: CapabilityInfo) {
+        wearNodesWithApp = capabilityInfo.nodes
+        findAllWearDevices()
+        verifyNodeAndStartCommunication()
     }
 
     override fun onOverviewFragmentInteraction(uri: Uri) {
@@ -141,4 +173,31 @@ class MainActivity :
             .commit()
     }
 
+
+    private fun findAllWearDevices() {
+        val nodeListTask: Task<List<Node>> = Wearable.getNodeClient(this).connectedNodes
+        nodeListTask.addOnCompleteListener {
+            if (it.isSuccessful) {
+                allConnectedNodes = it.result ?: Collections.emptyList()
+            }
+        }
+    }
+
+    private fun findWearDevicesWithApp() {
+        val capabilityInfoTask: Task<CapabilityInfo> = Wearable.getCapabilityClient(this)
+            .getCapability(CAPABILITY_WEAR_APP, CapabilityClient.FILTER_ALL)
+        capabilityInfoTask.addOnCompleteListener {
+            if (it.isSuccessful) {
+                val capabilityInfo: CapabilityInfo? = it.result
+                wearNodesWithApp = capabilityInfo?.nodes ?: Collections.emptySet()
+                verifyNodeAndStartCommunication()
+            }
+        }
+    }
+
+    private fun verifyNodeAndStartCommunication() {
+        if (!allConnectedNodes.isEmpty() && !wearNodesWithApp.isEmpty()) {
+            // TODO: Communicate with wear app
+        }
+    }
 }
