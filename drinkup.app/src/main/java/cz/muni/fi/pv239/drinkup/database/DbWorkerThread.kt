68b4906c2744cc5bb8597657c8aae1769c8a0e35
Martin Hamerník <445720@mail.muni.cz>
2019-04-27 16:01:05 +0200

    Load, save and update drink definitions to db

--- drinkup.app/src/main/java/cz/muni/fi/pv239/drinkup/database/DbWorkerThread.kt
+++ drinkup.app/src/main/java/cz/muni/fi/pv239/drinkup/database/DbWorkerThread.kt
@@ -4,14 +4,14 @@ import android.os.Handler
 import android.os.HandlerThread
 
 class DbWorkerThread(threadName: String) : HandlerThread(threadName) {
-    private lateinit var mWorkerHandler: Handler
+    private lateinit var workerHandler: Handler
 
     override fun onLooperPrepared() {
         super.onLooperPrepared()
-        mWorkerHandler = Handler(looper)
+        workerHandler = Handler(looper)
     }
 
     fun postTask(task: Runnable) {
-        mWorkerHandler.post(task)
+        workerHandler.post(task)
     }
 }
