7c89ee566760479976fb589c921770ef59c4aba4
Tatiana Zboncakova <445312@mail.muni.cz>
2019-05-16 23:10:19 +0200

    added layout for drinking session detail

--- drinkup.app/src/main/java/cz/muni/fi/pv239/drinkup/activity/DrinkingSessionDetailActivity.kt
+++ drinkup.app/src/main/java/cz/muni/fi/pv239/drinkup/activity/DrinkingSessionDetailActivity.kt
@@ -3,4 +3,11 @@ package cz.muni.fi.pv239.drinkup.activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class DrinkingSessionDetailActivity: AppCompatActivity()
+class DrinkingSessionDetailActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(cz.muni.fi.pv239.drinkup.R.layout.activity_drinking_session_detail)
+}
+
+}
