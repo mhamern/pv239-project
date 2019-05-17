4b70e48dd01523346fb89dc0e37ac02b0b9b8cad
Martin Hamerník <445720@mail.muni.cz>
2019-05-17 16:40:45 +0200

    Add dummy communication

--- drinkup.wearable/src/main/java/cz/muni/fi/pv239/drinkup/MainActivity.kt
+++ drinkup.wearable/src/main/java/cz/muni/fi/pv239/drinkup/MainActivity.kt
@@ -2,18 +2,12 @@ package cz.muni.fi.pv239.drinkup
 
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
-import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.wear.ambient.AmbientModeSupport
 import com.google.android.gms.tasks.Task
-import com.google.android.gms.wearable.CapabilityClient
-import com.google.android.gms.wearable.CapabilityInfo
-import com.google.android.gms.wearable.Node
-import com.google.android.gms.wearable.Wearable
+import com.google.android.gms.wearable.*
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-import com.google.android.gms.tasks.OnCompleteListener
-
 
 
 class MainActivity : WearableActivity(),
@@ -23,6 +17,9 @@ class MainActivity : WearableActivity(),
     companion object {
         @JvmStatic val CAPABILITY_PHONE_APP = "watch_server"
         @JvmStatic val ADD_DRINK_MESSAGE_PATH = "/add_drink"
+        @JvmStatic val GET_ALCOHOL_IN_BLOOD_REQUEST_PATH = "/alcohol"
+        @JvmStatic val GET_LAST_DRINK_NAME_REQUEST_PATH = "/last_drink"
+        @JvmStatic val CONFIRM_ADD_DRINK_REQUEST_PATH = "/drink_added"
     }
 
     private var androidPhoneNodeWithApp: Node? = null
@@ -30,8 +27,6 @@ class MainActivity : WearableActivity(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Wearable.getCapabilityClient(this).addListener(this, CAPABILITY_PHONE_APP)
-        checkIfPhoneHasApp()
         setAmbientEnabled()
         initAddDrinkButton()
     }
@@ -54,6 +49,26 @@ class MainActivity : WearableActivity(),
     override fun onCapabilityChanged(capabilityInfo: CapabilityInfo) {
         androidPhoneNodeWithApp = pickBestNodeId(capabilityInfo.nodes)
         verifyNodeAndUpdateUI()
+        startCommunication()
+    }
+
+    private fun startCommunication() {
+        if (androidPhoneNodeWithApp != null) {
+            Wearable.getMessageClient(this).addListener { messageEvent -> handleMessage(messageEvent) }
+            val id = androidPhoneNodeWithApp?.id
+            if (id != null) {
+                requestGetAlcoholInBlood(id)
+                requestGetLastDrink(id)
+            }
+        }
+    }
+
+    private fun handleMessage(messageEvent: MessageEvent) {
+        when (messageEvent.path) {
+            CONFIRM_ADD_DRINK_REQUEST_PATH -> onAddDrinkSuccess()
+            GET_LAST_DRINK_NAME_REQUEST_PATH -> onLastDrinkNameUpdated(messageEvent)
+            GET_ALCOHOL_IN_BLOOD_REQUEST_PATH -> onAlcoholInBloodUpdated(messageEvent)
+        }
     }
 
     private fun pickBestNodeId(nodes: Set<Node>): Node? {
@@ -87,18 +102,8 @@ class MainActivity : WearableActivity(),
                 androidPhoneNodeWithApp = pickBestNodeId(nodes)
             }
             verifyNodeAndUpdateUI()
+            startCommunication()
         }
-
-/*       val capabilityInfoTask: Task<CapabilityInfo> = Wearable.getCapabilityClient(this).getCapability(
-           CAPABILITY_PHONE_APP, CapabilityClient.FILTER_ALL)
-        capabilityInfoTask.addOnCompleteListener {
-            if (it.isSuccessful) {
-                val result = it.result
-                val nodes = result?.nodes ?: Collections.emptySet()
-                androidPhoneNodeWithApp = pickBestNodeId(nodes)
-            }
-            verifyNodeAndUpdateUI()
-        }*/
     }
 
     private fun initAddDrinkButton() {
@@ -117,13 +122,52 @@ class MainActivity : WearableActivity(),
                 null
             ).apply {
                 addOnSuccessListener {
-
                 }
                 addOnFailureListener {
-
+                    onAddDrinkFailure()
                 }
             }
         }
+    }
+
+    private fun requestGetLastDrink(id: String) {
+        id.also { nodeId ->
+            val sendTask: Task<*> = Wearable.getMessageClient(this).sendMessage(
+                nodeId,
+                GET_LAST_DRINK_NAME_REQUEST_PATH,
+                null
+            )
+        }
+    }
+
+    private fun requestGetAlcoholInBlood(id: String) {
+        id.also { nodeId ->
+            val sendTask: Task<*> = Wearable.getMessageClient(this).sendMessage(
+                nodeId,
+                GET_ALCOHOL_IN_BLOOD_REQUEST_PATH,
+                null
+            )
+        }
+    }
+
+
+    private fun onAlcoholInBloodUpdated(messageEvent: MessageEvent) {
+        val alcoholInBlood = messageEvent.data.toString(Charsets.UTF_8)
+        alcohol_in_blood_text.text = getString(R.string.alcohol_in_blood, alcoholInBlood)
+    }
+
+    private fun onLastDrinkNameUpdated(messageEvent: MessageEvent) {
+        val lastDrinkName = messageEvent.data.toString(Charsets.UTF_8)
+        add_drink_button.text = getString(R.string.add_last_drink_parameterized, lastDrinkName)
+    }
+
+    private fun onAddDrinkSuccess() {
+        Toast.makeText(this, getString(R.string.drink_added_toast), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onAddDrinkFailure() {
+        Toast.makeText(this, "Adding drink failed. Check you connection.", Toast.LENGTH_LONG).show()
+
     }
 
     private inner class MyAmbientCallback : AmbientModeSupport.AmbientCallback() {
